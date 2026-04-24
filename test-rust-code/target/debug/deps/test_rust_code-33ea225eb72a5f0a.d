fec762e0041ca32dbf49e33e4dd76f86c4ae7d3e
Sam Kuz <samuelkuz@gmail.com>
2026-04-23 22:15:07 -0700

    Stop tracking Rust target directory

--- test-rust-code/target/debug/deps/test_rust_code-33ea225eb72a5f0a.d
+++
@@ -1,5 +0,0 @@
-/Users/samkuz/Coding/semantic-codesense/test-rust-code/target/debug/deps/test_rust_code-33ea225eb72a5f0a.d: src/main.rs
-
-/Users/samkuz/Coding/semantic-codesense/test-rust-code/target/debug/deps/libtest_rust_code-33ea225eb72a5f0a.rmeta: src/main.rs
-
-src/main.rs:
